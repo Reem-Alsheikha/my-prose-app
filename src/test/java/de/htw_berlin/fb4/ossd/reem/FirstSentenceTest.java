652f3b75f67b2c305e8287ecbdf1a9e7bbef9974
Reem <Reemalsheikha5@gmail.com>
2026-07-06 16:32:33 +0200

    Add Apache 2.0 license headers to source files

--- src/test/java/de/htw_berlin/fb4/ossd/reem/FirstSentenceTest.java
+++ src/test/java/de/htw_berlin/fb4/ossd/reem/FirstSentenceTest.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2026 Reem Alsheikha
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package de.htw_berlin.fb4.ossd.reem;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
